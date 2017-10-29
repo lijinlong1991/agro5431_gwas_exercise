231307915a9fe65498df138bde273969337bf002
mcnis003 <mcnis003@umn.edu>
2017-10-29 15:20:46 -0500

    update
    change memory requirements on qsub

--- gwas_qsub.sh
+++ gwas_qsub.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#PBS -l walltime=01:00:00,nodes=1:ppn=16,pmem=5gb
-#PBS -o /home/agro5431/mcnis003/gwas_exercise/gwas_o
-#PBS -e /home/agro5431/mcnis003/gwas_exercise/gwas_e
+#PBS -l walltime=01:00:00,nodes=1:ppn=16,pmem=32gb
+#PBS -o /home/agro5431/<user>/gwas_exercise/gwas_o
+#PBS -e /home/agro5431/<user>/gwas_exercise/gwas_e
 #PBS -N gwas
 #PBS -q ram256g
+#PBS -m abe
+#PBS -M <user>@umn.edu
 
-cd /home/agro5431/mcnis003/gwas_exercise
+cd /home/agro5431/<user>/gwas_exercise
 ln -s /home/agro5431/shared/GAPIT.RNAseq.hmp_438K_imputed2_no_quotes.csv gwas_genotypes.csv
 ln -s /home/agro5431/shared/tpc119982SupplementalDS6.csv gwas_phenotypes.csv
 module load perl
 perl convert_for_rrblup.pl -i gwas_genotypes.csv -o genotypes_rrblup.csv -f 12 -l 514
 module load R/3.3.3
-Rscript gwas.R /home/agro5431/mcnis003/gwas_exercise/ genotypes_rrblup.csv gwas_phenotypes.csv 16 1 503 FALSE
+Rscript gwas.R /home/agro5431/<user>/gwas_exercise/ genotypes_rrblup.csv gwas_phenotypes.csv 16 1 503 FALSE
